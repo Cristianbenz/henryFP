192096d5808e0467f385e5a3fd67bc8c0be0de0d
Cristianbenz <contactocbenitez@gmail.com>
2022-11-14 10:10:52 -0300

    update postgres connect for deploy

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const { generateKey } = require('crypto');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  PGUSER,
+  PGPASSWORD,
+  PGHOST,
+  PGPORT,
+  PGDATABASE
 } = process.env;
 // cambiar name de db
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgresql://${PGUSER}}:${PGPASSWORD}}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   // CONFIGURCION ADICIONAL
